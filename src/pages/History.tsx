8e9f0c745e610748d4f8cc409991793b3dcc6318
hmduc15 <hmduc.m10@gmail.com>
2023-05-28 16:08:29 +0700

    add folder page router

---
+++ src/pages/History.tsx
@@ -0,0 +1,53 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import { FunctionComponent, useEffect, useState } from "react";
+import Title from "../components/Common/Title";
+import FilmListViewForBookmarkAndHistory from "../components/FilmListViewForBookmarkAndHistory/FilmListViewForBookmarkAndHistory";
+import { db } from "../shared/firebase";
+import { Item } from "../shared/types";
+import { useAppSelector } from "../store/hooks";
+
+interface HistoryProps {}
+
+const History: FunctionComponent<HistoryProps> = () => {
+  const currentUser = useAppSelector((state) => state.auth.user);
+  const [recentlyWatchFilms, setRecentlyWatchFilms] = useState<Item[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(
+    !Boolean(recentlyWatchFilms.length)
+  );
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    if (!currentUser) return;
+
+    const unsubDoc = onSnapshot(
+      doc(db, "users", currentUser?.uid),
+      (doc) => {
+        setRecentlyWatchFilms(doc.data()?.recentlyWatch.slice().reverse());
+        setIsLoading(false);
+      },
+      (error) => {
+        alert(error);
+        setRecentlyWatchFilms([]);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    );
+
+    return () => unsubDoc();
+  }, [currentUser]);
+
+  if (isError) return <div>ERROR</div>;
+
+  return (
+    <>
+      <Title value="History | Moonlight" />
+      <FilmListViewForBookmarkAndHistory
+        films={recentlyWatchFilms}
+        isLoading={isLoading}
+        pageType="history"
+      />
+    </>
+  );
+};
+
+export default History;
